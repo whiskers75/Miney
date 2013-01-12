e11805e68df888eb6ed400f6a599baa7b6033e7f
whiskers75 <crazycats@yopmail.com>
2013-01-12 05:06:03 -0500

    coins.js does not have a constructor

--- test/coins.js
+++ test/coins.js
@@ -1,16 +1,15 @@
 // Test Coins Object
 var coins = require(__dirname + '/../lib/coins.js');
-var Coins = coins.coins();
 describe('coins', function() {
     it('should return an object', function() {
-        return new Coins();
+        return coins.coins;
     });
     it('should return 0 as the starting balance', function() {
-        return new Coins().balance;
+        return coins.coins.balance;
     });
     it('should add 1 onto the balance and return 1', function() {
-        var n = 0;
-        new Coins().alter(1, n);
+        var n = 0
+        coins.coins.alter(1, n);
         return n;
     });
 })

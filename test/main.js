343fb46ffaa0a4ea62c6969fad31f920f9797691
whiskers75 <crazycats@yopmail.com>
2013-01-12 04:53:28 -0500

    Fix tests 2

--- test/main.js
+++ test/main.js
@@ -1,7 +1,12 @@
 // Main File Tests
 var main = require(__dirname + '/../main.js')
 describe('main', function() {
-    it('throws an error when the output cannot be reached', function() {
-        main.testOut('');
+    it('should return an error when the output cannot be reached', function() {
+        try {
+            main.testOut('');
+        }
+        catch (e) {
+            return e;
+        };
     });
 });

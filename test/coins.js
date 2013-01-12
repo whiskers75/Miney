88f5712fc3b9a99f2d176d9af8afc6ff5cf454db
whiskers75 <crazycats@yopmail.com>
2013-01-12 05:02:43 -0500

    Fixed tests (maybe)

--- test/coins.js
+++ test/coins.js
@@ -1,15 +1,16 @@
 // Test Coins Object
 var coins = require(__dirname + '/../lib/coins.js');
+var Coins = coins.coins();
 describe('coins', function() {
     it('should return an object', function() {
-        return new coins.coins();
+        return new Coins();
     });
     it('should return 0 as the starting balance', function() {
-        return new coins.coins().balance;
+        return new Coins().balance;
     });
     it('should add 1 onto the balance and return 1', function() {
         var n = 0;
-        new coins.coins().alter(1, n);
+        new Coins().alter(1, n);
         return n;
     });
 })
